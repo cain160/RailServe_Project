38b1a92e168ee5f874d61f3bff77186137b08fd2
CNate <nvelliquette1@gmail.com>
2017-03-14 07:08:48 +0000

    Further test Flask

--- py/server2.py
+++ py/server2.py
@@ -4,7 +4,19 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+    return render_template('index.html')
+
+@app.route('/details1.html')
+def det_page1():
     return render_template('details1.html')
+
+@app.route('/details2.html')
+def det_page2():
+    return render_template('details2.html')
+
+@app.route('/details3.html')
+def det_page3():
+    return render_template('details3.html')
 
 if __name__ == '__main__':
     app.run(port=int("5000"), host='192.168.1.187', debug=True)
