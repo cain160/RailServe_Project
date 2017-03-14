452168a7fd7a7afb895c0b1dc0cddc063c9d7afb
CNate <nvelliquette1@gmail.com>
2017-03-14 19:47:33 -0400

    Deploy Clay's changes on Flask

--- py/server2.py
+++ py/server2.py
@@ -3,7 +3,8 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def index():
+@app.route('/index.html')
+def home():
     return render_template('index.html')
 
 @app.route('/details1.html')
